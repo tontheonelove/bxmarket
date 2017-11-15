2b402ee307c5e4719786c10384f8d4296e43d4ba
tontheonelove <tonlove5233@gmail.com>
2017-11-15 11:11:53 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $ch = curl_init("https://bx.in.th/api/"); // add your url which contains json file
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $content = curl_exec($ch);
+    curl_close($ch);
+    $json = json_decode($content, true);
+    //print_R($json);
+    $count=count($json);
+    echo'<table><th>เหรียญ</th><th>อัตราขึ้น-ลงวันนี้</th><th>ราคาล่าสุด  (บาท)</th>';
+    for($i=0;$i<$count;$i++)
+    {
+      echo'<tr><td>'.$json[$i]['secondary_currency'].'</td><td>'.$json[$i]['change'].'</td><td>'.$json[$i]['last_price'].'</td></tr>';
+    }
+    echo'</table>';
+
+?>
